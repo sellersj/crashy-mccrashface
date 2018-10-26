e2c4bce60b6526d711b667d8e8c79c5db7bf1182
Jim Sellers <jim.sellers@gmail.com>
2018-10-25 20:37:51 -0400

    changing all crypto stuff to happen in thread since I don't think it's thread safe

--- src/main/java/com/example/crashymccrashface/LoadGeneratorImpl.java
+++ src/main/java/com/example/crashymccrashface/LoadGeneratorImpl.java
@@ -1,38 +1,27 @@
 package com.example.crashymccrashface;
 
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
 import java.util.Random;
 
 import javax.crypto.Cipher;
 import javax.crypto.KeyGenerator;
+import javax.crypto.NoSuchPaddingException;
 import javax.crypto.SecretKey;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.InitializingBean;
 import org.springframework.stereotype.Component;
 import org.springframework.util.StopWatch;
 
 /** The whole idea of this class is to just introduce cpu load. */
 @Component
-public class LoadGeneratorImpl implements LoadGenerator, InitializingBean {
+public class LoadGeneratorImpl implements LoadGenerator {
 
     private final Logger logger = LoggerFactory.getLogger(LoadGeneratorImpl.class);
 
     /** For generating random bytes. */
     private final Random random = new Random();
-
-    /** The cipher to use to generate load. */
-    private Cipher cipher;
-
-    @Override
-    public void afterPropertiesSet() throws Exception {
-        KeyGenerator kgen = KeyGenerator.getInstance("AES");
-        kgen.init(128);
-        SecretKey aesKey = kgen.generateKey();
-
-        cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
-        cipher.init(Cipher.ENCRYPT_MODE, aesKey);
-    }
 
     @Override
     public void generateLoad() {
@@ -59,17 +48,30 @@ public class LoadGeneratorImpl implements LoadGenerator, InitializingBean {
 
         @Override
         public void run() {
-            StopWatch watch = new StopWatch();
-            watch.start();
-            do {
-                byte[] array = new byte[Integer.MAX_VALUE / 12000];
-                random.nextBytes(array);
-                cipher.update(array);
-            } while (System.currentTimeMillis() < timeToStop);
+            try {
+                KeyGenerator kgen = KeyGenerator.getInstance("AES");
 
-            watch.stop();
-            // not sure why, but this doesn't log in the test case
-            logger.info("Finished running a load for " + watch.getTotalTimeSeconds());
+                kgen.init(128);
+                SecretKey aesKey = kgen.generateKey();
+
+                Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
+                cipher.init(Cipher.ENCRYPT_MODE, aesKey);
+
+                StopWatch watch = new StopWatch();
+                watch.start();
+                do {
+                    byte[] array = new byte[Integer.MAX_VALUE / 12000];
+                    random.nextBytes(array);
+                    cipher.update(array);
+                } while (System.currentTimeMillis() < timeToStop);
+
+                watch.stop();
+                // not sure why, but this doesn't log in the test case
+                logger.info("Finished running a load for " + watch.getTotalTimeSeconds());
+
+            } catch (NoSuchAlgorithmException | NoSuchPaddingException | InvalidKeyException e) {
+                throw new RuntimeException("Something went wrong initalizing crypto", e);
+            }
         }
     }
 }
