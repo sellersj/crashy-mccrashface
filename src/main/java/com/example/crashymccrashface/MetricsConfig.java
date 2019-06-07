d276ff9bf61573e9831176ba7e9929139bbbbef3
Jim Sellers <jim.sellers@canada.ca>
2019-06-07 15:13:05 -0400

    trying the new config for micrometer

--- src/main/java/com/example/crashymccrashface/MetricsConfig.java
+++ src/main/java/com/example/crashymccrashface/MetricsConfig.java
@@ -1,36 +1,31 @@
 package com.example.crashymccrashface;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.actuate.autoconfigure.metrics.MeterRegistryCustomizer;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
-import io.micrometer.core.instrument.MeterRegistry;
-
 /**
+ *
+ * Since Micrometer 1.1.0 with a property: <code>management.metrics.tags.application=${spring.application.name}</code>
+ *
+ * Since we're using a newer version, the rest of this class is not needed. Leaving in place for older applications.
  *
  * @author sellersj
  */
-@Configuration
+// @Configuration
 public class MetricsConfig {
 
-    private Logger logger = LoggerFactory.getLogger(MetricsConfig.class);
-
-    /** Application name. Defined in application.propreties. */
-    @Value("${spring.application.name}")
-    String appName;
-
-    /**
-     * Set any required "common tags".
-     *
-     * @return MeterRegistryCustomizer<MeterRegistry>
-     */
-    @Bean
-    public MeterRegistryCustomizer<MeterRegistry> metricsCommonTags() {
-        logger.info("App name: " + appName);
-        return registry -> registry.config().commonTags("application", appName);
-    }
+    // private Logger logger = LoggerFactory.getLogger(MetricsConfig.class);
+    //
+    // /** Application name. Defined in application.propreties. */
+    // @Value("${spring.application.name}")
+    // String appName;
+    //
+    // /**
+    // * Set any required "common tags".
+    // *
+    // * @return MeterRegistryCustomizer<MeterRegistry>
+    // */
+    // @Bean
+    // public MeterRegistryCustomizer<MeterRegistry> metricsCommonTags() {
+    // logger.info("App name: " + appName);
+    // return registry -> registry.config().commonTags("application", appName);
+    // }
 
 }
