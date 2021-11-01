307bd5c0efcbc803d3ba50083f0ab7a2b7469066
Jim Sellers <jim.sellers@gmail.com>
2021-11-01 09:15:18 -0400

    adding an assert to the test

--- src/test/java/com/example/crashymccrashface/LoadGeneratorTest.java
+++ src/test/java/com/example/crashymccrashface/LoadGeneratorTest.java
@@ -1,5 +1,7 @@
 package com.example.crashymccrashface;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -12,7 +14,11 @@ public class LoadGeneratorTest {
 
     @Test
     public void generateLoadSmokeTest() {
+        long start = System.currentTimeMillis();
         loadGenerator.generateLoad(1);
+        long end = System.currentTimeMillis();
+        assertTrue(end > start,
+            String.format("should have taken some time to run the tests. start: %s end %s", start, end));
     }
 
 }
